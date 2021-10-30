e56815f221181b83b86bedb1a2f4414b6498c04c
Alejandro Medina <drolejan@gmail.com>
2021-10-30 15:08:39 -0500

    Mas caracteres

--- generador_contrasena.py
+++ generador_contrasena.py
@@ -2,10 +2,10 @@ import random
 
 
 def generar_contrasena():
-    mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    simbolos = ['!', '#', '$', '&', '/', '(', ')']
+    mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
     numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    simbolos = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
 
     caracteres = mayusculas + minusculas + simbolos + numeros
 
